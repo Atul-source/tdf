d950ef2a9b8c8aef96debf7d6ba783bb6479dd4c
Atul-source <atulprajapati6031@gmail.com>
2023-12-06 14:48:57 +0530

    testing
    Signed-off-by: Atul-source <atulprajapati6031@gmail.com>

--- a.cpp
+++ a.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
   int z,p;
+  vector<int> v;
+  v.push_back(1);
+  cout << v.size() << endl;
   cout << z + z + 2 << endl;
 }
